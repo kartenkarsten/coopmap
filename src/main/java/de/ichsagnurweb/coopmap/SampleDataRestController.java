e88b4fd01f558413ac7bc28d369346e574ece2c8
Karsten <kartenkarsten@ichsagnurweb.de>
2024-05-03 23:02:33 +0200

    added a rest endpoint to create sample data for a map

---
+++ src/main/java/de/ichsagnurweb/coopmap/SampleDataRestController.java
@@ -0,0 +1,23 @@
+package de.ichsagnurweb.coopmap;
+
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleDataRestController {
+
+    @Autowired
+    private MarkerRepository markerRepository;
+
+    @PostMapping("/api/map/{mapId}/sampledata")
+    void createSampleData(@PathVariable(value="mapId") String mapId) {
+//        assert "internal-test-map".equals(mapId);
+
+        Marker bahnhof = new Marker(mapId, "Bahnhof Lüneburg", 10.41954, 53.24989);
+        markerRepository.save(bahnhof);
+    }
+}
---
+++ src/test/java/de/ichsagnurweb/coopmap/SampleDataRestControllerTest.java
@@ -0,0 +1,45 @@
+package de.ichsagnurweb.coopmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles(value = "test")
+public class SampleDataRestControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private MarkerRepository markerRepository;
+
+    @Test
+    public void testUserController() throws Exception {
+        String mapId = "test";
+
+        // Define the expected result or behavior
+        String expectedResponse = "";
+
+
+        assertThat(markerRepository.findAllByMapId(mapId)).isEmpty();
+
+        // Perform GET request and verify the response
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/map/"+mapId+"/sampledata")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+               .andExpect(MockMvcResultMatchers.content().string(expectedResponse));
+
+        assertThat(markerRepository.findAllByMapId(mapId)).hasSize(1);
+    }
+
+}
